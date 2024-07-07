4734f538f540451a5d9460af2e0eeeb97e4d466f
joey aronson <aronsonjoey@gmail.com>
2024-07-07 00:46:17 -0700

    resume update

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -9,7 +9,7 @@ export default function Resume() {
             <div className="resumeContainer">
                 <iframe
                     title="resume"
-                    src="https://drive.google.com/file/d/152SGE5_XpSdPP-A1rNRRKUlgFmhpscOI/preview"
+                    src="https://docs.google.com/document/d/e/2PACX-1vTFvTJUTHWni7iEyDtV_WYpGCG5FU7-2m3YbMJmICFidI-tYCqnjEOvlCjYb8x_QA/pub?embedded=true"
                 ></iframe>
             </div>
         </>
