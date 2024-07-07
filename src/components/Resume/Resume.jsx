fbf2fba1fa652dd03bc163bafcab9dee157eb91a
joey aronson <aronsonjoey@gmail.com>
2024-07-07 00:41:42 -0700

    title

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 export default function Resume() {
     return (
-        <div className="resumeContainer">
-            <iframe
-                title="resume"
-                src="https://drive.google.com/file/d/152SGE5_XpSdPP-A1rNRRKUlgFmhpscOI/preview"
-            ></iframe>
-        </div>
+        <>
+            <div className="nameHeader">
+                <p className="name">RESUME</p>
+            </div>
+            <div className="resumeContainer">
+                <iframe
+                    title="resume"
+                    src="https://drive.google.com/file/d/152SGE5_XpSdPP-A1rNRRKUlgFmhpscOI/preview"
+                ></iframe>
+            </div>
+        </>
     );
 }
